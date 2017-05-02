c2cd3ab7ac38c81e15a348991e430aa55bd6d754
rztiwari <rztiwari@gmail.com>
2017-05-03 00:09:07 +0100

    Updated services

--- routes/presentation.js
+++ routes/presentation.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var presetationRoute = express.Router();
-var path = require('path');
-var fs = require('fs');
-var bodyParser = require('body-parser');
+const express = require('express');
+const presetationRoute = express.Router();
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
-var jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
 
 // create application/json parser
-var jsonParser = bodyParser.json();
+const jsonParser = bodyParser.json();
 
-var slideListFile = path.join(process.cwd() + '/data/slideList.json');
-var slideDataFile = path.join(process.cwd() + '/data/slideData.json');
+const slideListFile = path.join(process.cwd() + '/data/slideList.json');
+const slideDataFile = path.join(process.cwd() + '/data/slideData.json');
 
 presetationRoute.get('/slideList', function(req, res) {
   jsonfile.readFile(slideListFile, function(err, obj) {
@@ -28,26 +28,7 @@ presetationRoute.get('/slideList', function(req, res) {
 });
 
 presetationRoute.get('/slideData/:id', function(req, res) {
-  var slideId = req.params && req.params['id']
-    ? req.params['id']
-    : null;
-  jsonfile.readFile(slideDataFile, function(err, obj) {
-
-    res.set("Content-Type", "application/json");
-    if (err) {
-      res.status(500).json({error: err});
-    }
-
-    if (obj && slideId && obj[slideId]) {
-      res.status(200).json(obj[slideId]);
-    } else {
-      res.status(200).json({data: []});
-    }
-  });
-});
-
-presetationRoute.get('/slideData/:id', function(req, res) {
-  var slideId = req.params && req.params['id']
+  let slideId = req.params && req.params['id']
     ? req.params['id']
     : null;
   jsonfile.readFile(slideDataFile, function(err, obj) {
@@ -66,7 +47,7 @@ presetationRoute.get('/slideData/:id', function(req, res) {
 });
 
 presetationRoute.delete('/slideData/:id', function(req, res) {
-  var slideId = req.params && req.params['id']
+  let slideId = req.params && req.params['id']
     ? req.params['id']
     : null;
   jsonfile.readFile(slideDataFile, function(err, obj) {
@@ -74,6 +55,7 @@ presetationRoute.delete('/slideData/:id', function(req, res) {
     res.set("Content-Type", "application/json");
     if (err) {
       res.status(500).json({error: err});
+      return;
     }
 
     if (obj && slideId && obj[slideId]) {
@@ -82,7 +64,15 @@ presetationRoute.delete('/slideData/:id', function(req, res) {
       jsonfile.writeFile(slideDataFile, obj, function(err, success) {
         if (err) {
           res.status(500).json({error: err});
+          return;
         }
+        fileListData = jsonfile.readFileSync(slideListFile);
+        if (fileListData && fileListData.data && fileListData.data.length > 0) {
+          fileListData.data = fileListData.data.filter(function(item) {
+            return item !== slideId;
+          });
+        }
+        jsonfile.writeFileSync(slideListFile, fileListData);
         res.status(200).json(delData);
       });
     } else {
@@ -92,10 +82,10 @@ presetationRoute.delete('/slideData/:id', function(req, res) {
 });
 
 presetationRoute.post('/slideData/:id', jsonParser, function(req, res) {
-  var slideId = req.params['id']
+  let slideId = req.params['id']
     ? req.params['id']
     : null;
-  var fileStats = fs.statSync(slideDataFile);
+  let fileStats = fs.statSync(slideDataFile);
   if (fileStats && fileStats.size > 0) {
     obj = jsonfile.readFileSync(slideDataFile);
   } else {
@@ -107,42 +97,61 @@ presetationRoute.post('/slideData/:id', jsonParser, function(req, res) {
   slideData = req.body;
   if (slideData && slideId && !obj[slideId]) {
     obj[slideId] = slideData[slideId];
-  }else{
+  } else {
     res.status(500).json({'error': 'Can\'n add the record'});
     return;
   }
-  jsonfile.writeFile(slideDataFile, obj, function(err, success) {
-    if (err) {
-      res.status(500).json({error: err});
-      return;
-    }
-    res.status(200).json(req.body);
-  });
+
+  writeDataToFile(obj, slideId, req, res);
 });
 
 presetationRoute.put('/slideData/:id', jsonParser, function(req, res) {
-  var slideId = req.params['id']
+  let slideId = req.params['id']
     ? req.params['id']
     : null;
-  var fileStats = fs.statSync(slideDataFile);
+  let fileStats = fs.statSync(slideDataFile);
   if (fileStats && fileStats.size > 0) {
     obj = jsonfile.readFileSync(slideDataFile);
   } else {
     obj = {};
   }
 
-  res.set("Content-Type", "application/json");
-
   slideData = req.body;
   if (slideData && slideId) {
     obj[slideId] = slideData[slideId];
   }
-  jsonfile.writeFile(slideDataFile, obj, function(err, success) {
+  writeDataToFile(obj, slideId, req, res);
+});
+
+const writeDataToFile = function(json, slideId, req, res) {
+  let fileListData,
+    fileStats = fs.statSync(slideListFile);
+  if (fileStats && fileStats.size > 0) {
+    fileListData = jsonfile.readFileSync(slideListFile);
+  } else {
+    fileListData = {};
+  }
+  jsonfile.writeFile(slideDataFile, json, function(err, success) {
     if (err) {
       res.status(500).json({error: err});
+      return;
     }
+
+    if (fileListData && fileListData.data && Array.isArray(fileListData.data)) {
+      if (fileListData.data.indexOf(slideId) === -1) {
+        fileListData.data.push(slideId);
+      }
+    } else {
+      fileListData = {};
+      fileListData.data = [];
+      fileListData.data.push(slideId);
+    }
+
+    jsonfile.writeFileSync(slideListFile, fileListData);
+
+    res.set("Content-Type", "application/json");
     res.status(200).json(req.body);
   });
-});
+}
 
 module.exports = presetationRoute;
